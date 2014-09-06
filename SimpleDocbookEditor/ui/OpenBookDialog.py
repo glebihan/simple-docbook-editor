044913cd0fa73eeb816c1b49d1f26ed2ec5e0baf
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2014-09-06 15:51:06 +0200

    Added header to all files

--- SimpleDocbookEditor/ui/OpenBookDialog.py
+++ SimpleDocbookEditor/ui/OpenBookDialog.py
@@ -1,4 +1,22 @@
-# -*- coding=utf-8 -*-
+# OpenBookDialog.py
+# -*- Mode: Python; indent-tabs-mode: nil; tab-width: 4; coding: utf-8 -*-
+#
+# Copyright © 2014 Gwendal Le Bihan
+# 
+# This file is part of Simple DocBook Editor.
+# 
+# Simple DocBook Editor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Simple DocBook Editor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Simple DocBook Editor.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 
